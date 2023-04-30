c6513850e4aa8b3b6e811aa58efcbcc685fe9485
Omid Noorshams <o.noorshams@gmail.com>
2023-04-30 15:39:03 -0700

    add login logout and user onfo in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,31 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Home() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div className="">
       <h1>Home</h1>
       <div className="">
-        <Link href="/api/auth/login">Login</Link>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                height={50}
+                width={50}
+                priority
+              />
+              <div className="">{user.email}</div>
+            </div>
+            <Link href="/api/auth/logout">logout</Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
       </div>
     </div>
   );
