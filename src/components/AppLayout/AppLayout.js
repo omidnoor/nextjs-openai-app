53532f4a21732dc368940f737a33a73417bb63ae
Omid Noorshams <o.noorshams@gmail.com>
2023-04-30 16:27:52 -0700

    add tailwind to page layout

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,8 +1,18 @@
-export default function AppLayout({ Children }) {
+export default function AppLayout({ children }) {
   return (
-    <div>
-      AppLayout
-      <div>{Children}</div>
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800">
+          <div>logo</div>
+          <div>cta btn</div>
+          <div>token</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          list of posts
+        </div>
+        <div className="bg-cyan-800">user info - logout btn</div>
+      </div>
+      <div className="">{children}</div>
     </div>
   );
 }
