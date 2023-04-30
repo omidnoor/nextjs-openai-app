a5fada45fa783d304a3037db6a941ac62e336e4f
Omid Noorshams <o.noorshams@gmail.com>
2023-04-30 16:38:23 -0700

    create user onfo section in layout

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,4 +1,10 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function AppLayout({ children }) {
+  const { user } = useUser();
+
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
@@ -10,7 +16,30 @@ export default function AppLayout({ children }) {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
         </div>
-        <div className="bg-cyan-800">user info - logout btn</div>
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+          {!!user ? (
+            <>
+              <div className="min-w-[50px]">
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  height={50}
+                  width={50}
+                  priority
+                  className="rounded-full"
+                />
+              </div>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link href="/api/auth/logout" className="text-sm">
+                  logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </div>
       </div>
       <div className="">{children}</div>
     </div>
