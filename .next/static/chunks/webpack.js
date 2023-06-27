8955bd567908c882f91bef3fdaac92c2a182b080
Omid Noorshams <o.noorshams@gmail.com>
2023-06-27 14:42:20 -0700

    backto localhost base url

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b313eef080259472"; }
+/******/ 		__webpack_require__.h = function() { return "5fcc493dfa099b73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
